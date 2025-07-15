3343102b4ce5da4784ce50600b596bbcd4fd9781
Muhammad Ali <muhammad.ali.bright@gmail.com>
2025-07-15 01:32:12 -0500

    123

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,17 +5,22 @@ const admin = require('../../firebase/firebase')
 exports.register = async (req, res) => {
   const { role, firstName, lastName } = req.body
   const { uid, email } = req.user || {}
+  console.log('11111111111111111', { uid, email, role, firstName, lastName })
 
   if (!email || !uid) {
     return res.status(400).json({ error: 'Invalid user token' })
   }
 
   try {
+    console.log('2222222222222222', uid, role)
     await admin.auth().setCustomUserClaims(uid, { role })
+    console.log('3333333333333333', uid, role)
 
     const user = await prisma.user.create({
-      data: { id: uid, email, role, firstName, lastName, password: null } // Password is not stored, as per the schema
+      data: { id: uid, email, role, firstName, lastName } // Password is not stored, as per the schema
     })
+
+    console.log('4444444444444444', user)
 
     res.status(201).json({ message: 'User registered successfully!', user })
   } catch (err) {
